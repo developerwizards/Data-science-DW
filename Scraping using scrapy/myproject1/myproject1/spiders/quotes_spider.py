10a68908855490212d849bd8aab85bf187be1d09
Amardeep Gupta <amardeepg35@gmail.com>
2021-03-04 23:07:13 +0530

    scraped all quotes into json file

--- Scraping using scrapy/myproject1/myproject1/spiders/quotes_spider.py
+++ Scraping using scrapy/myproject1/myproject1/spiders/quotes_spider.py
@@ -6,7 +6,7 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            #'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -26,6 +26,10 @@ class QuotesSpider(scrapy.Spider):
                 'author':author,
                 'tags':tags
                 }
+            next_page = response.css('li.next a::attr(href)').get()
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback = self.parse)
         #with open(filename, 'wb') as f:
            # f.write(response.body)
         #self.log(f'Saved file {filename}')
